ceb6d9e0c12f76e6877d09f43e4eaee953efa22c
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-11-26 23:57:09 -0300

    chore: create base structure calculator component.

--- calculadora/src/pages/Calculator/index.js
+++ calculadora/src/pages/Calculator/index.js
@@ -9,6 +9,13 @@ export default class Calculator extends Component {
 
   constructor(props){
     super(props);
+  }
 
+  render(){
+    return (
+      <div className="calculator">
+
+      </div>
+    )
   }
 }
